8edff0fd64cea4ea947974618575fd7180946d8a
root <davi2812@hotmail.com>
2025-11-22 11:23:27 +0000

    added docker entrypoint to switch sensor app. removed icm from rplidr

--- src/rplidar_publisher.cc
+++ src/rplidar_publisher.cc
@@ -1,42 +1,12 @@
 #include "file/File.hh"
-#include "imu/icm-20948.h"
-#include "imu/icm-20948_defs.h"
 #include "lidar/RPLidar.hh"
 #include "recorder/ScanRecorder.hh"
 #include "sensors_server.hh"
-#include "timing/timing.hh"
 #include <filesystem>
-#include <future>
 #include <getopt.h>
 #include <iostream>
 #include <thread>
 
-// 100 Hz
-void ImuLoop(SensorsServer &server, msensor::ScanRecorder &recorder) {
-  constexpr uint64_t sample_period_us = 10000; // 100 hz
-  std::cout << "Start imu loop" << std::endl;
-  msensor::ICM20948 icm20948(1, ICM20948_ADDR0);
-  icm20948.init();
-
-  icm20948.calibrate();
-  while (true) {
-
-    const auto now = timing::getNowUs();
-
-    auto data = icm20948.getImuData();
-
-    recorder.record(data);
-    server.publishImu(data);
-
-    const auto remaining_us = sample_period_us - (timing::getNowUs() - now);
-    if (remaining_us > 0) {
-      std::this_thread::sleep_for(std::chrono::microseconds(remaining_us));
-    } else {
-      std::cout << "IMU loop overrun by " << -remaining_us << " us"
-                << std::endl;
-    }
-  }
-}
 void print_usage() {
   std::cout << "Usage: rplidar_publisher [serial device path]  [-r record] "
             << std::endl;
@@ -78,17 +48,17 @@ int main(int argc, char **argv) {
   SensorsServer server;
   server.start();
 
-  auto imu_loop = std::async(std::launch::async, [&server, &recorder]() {
-    ImuLoop(server, recorder);
-  });
-
   while (true) {
-    const auto scan = lidar->getScan();
-    std::cout << "New Scan @ " << scan->timestamp
-              << " Points: " << scan->points->size() << std::endl;
 
-    recorder.record(scan);
-    server.publishScan(scan);
+    if (const auto scan = lidar->getScan()) {
+      std::cout << "New Scan @ " << scan->timestamp
+                << " Points: " << scan->points->size() << std::endl;
+
+      recorder.record(scan);
+      server.publishScan(scan);
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
 
   server.stop();
--- test/client/client_imu.py
+++ test/client/client_imu.py
@@ -2,12 +2,16 @@ import grpc
 from proto_gen import sensors_pb2_grpc, sensors_pb2
 
 def main():
-    with grpc.insecure_channel('192.168.3.232:50051') as channel:
+    with grpc.insecure_channel('192.168.3.251:50052') as channel:
         stub = sensors_pb2_grpc.SensorServiceStub(channel)
-        request = sensors_pb2.SensorStreamRequest(queue_size=100)
+        request = sensors_pb2.SensorStreamRequest(queue_size=1)
         stream = stub.getImu(request)
+        lastT = 0
         for imu in stream:
-            print(f"Got imu: {imu}")
+            # print(f"Got imu: {imu}")
+            deltaT = imu.timestamp - lastT
+            print(f" DeltaT: {deltaT} ms")
+            lastT = imu.timestamp
 
 if __name__ == "__main__":
     main()
