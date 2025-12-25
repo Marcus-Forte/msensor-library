b7d0f5eb32a6bd7e8271a4b20fb205741b5f974c
Marcus Forte <davi2812@hotmail.com>
2025-12-25 14:19:42 +0100

    removed old scripts

--- test/client/client_imu.py
+++ test/client/client_imu.py
@@ -3,8 +3,12 @@ import argparse
 import grpc
 from proto_gen import sensors_pb2_grpc, sensors_pb2
 
+import viser
+import viser.uplot
 
-DEFAULT_SERVER_ADDR = "192.168.3.251:50052"
+import numpy as np
+
+DEFAULT_SERVER_ADDR = "localhost:50051"
 
 
 def main():
@@ -12,15 +16,96 @@ def main():
     parser.add_argument("--server", default=DEFAULT_SERVER_ADDR, help="gRPC target host:port")
     args = parser.parse_args()
 
+    server = viser.ViserServer()
+
+    time_step = 1.0 / 60.0
+    num_timesteps = 100
+    num_signals = 3  # ax, ay, az
+
+    x_data = time_step * np.arange(num_timesteps, dtype=np.float64)
+    y_acc = np.zeros((num_signals, num_timesteps))
+    y_gyr = np.zeros((num_signals, num_timesteps))
+    data_gyro = (x_data, *y_gyr)
+    data_acc = (x_data, *y_acc)
+
+    print("Data shapes:", [arr.shape for arr in data_acc])
+
+    uplot_handles: list[viser.GuiUplotHandle] = []
+    uplot_handles.append(
+        server.gui.add_uplot(
+            data=data_acc,
+            series=(
+                viser.uplot.Series(label="time"),
+                *[
+                    viser.uplot.Series(
+                        label=f"y{i}",
+                        stroke=["red", "green", "blue"][i % 3],
+                        width=2,
+                    )
+                    for i in range(num_signals)
+                ],
+            ),
+            title="Accelerometer",
+            scales={
+                "x": viser.uplot.Scale(
+                    time=False,
+                    auto=True,
+                ),
+                "y": viser.uplot.Scale(auto=True),
+            },
+            legend=viser.uplot.Legend(show=True),
+            aspect=2.0,
+        )
+    )
+
+    uplot_handles.append(
+        server.gui.add_uplot(
+            data=data_gyro,
+            series=(
+                viser.uplot.Series(label="time"),
+                *[
+                    viser.uplot.Series(
+                        label=f"y{i}",
+                        stroke=["red", "green", "blue"][i % 3],
+                        width=2,
+                    )
+                    for i in range(num_signals)
+                ],
+            ),
+            title="Gyro",
+            scales={
+                "x": viser.uplot.Scale(
+                    time=False,
+                    auto=True,
+                ),
+                "y": viser.uplot.Scale(auto=True),
+            },
+            legend=viser.uplot.Legend(show=True),
+            aspect=2.0,
+        )
+    )
+
     with grpc.insecure_channel(args.server) as channel:
         stub = sensors_pb2_grpc.SensorServiceStub(channel)
         request = sensors_pb2.SensorStreamRequest(queue_size=1)
         stream = stub.getImu(request)
         lastT = 0
+        sample: int = 0
         for imu in stream:
             deltaT = imu.timestamp - lastT
             print(f" DeltaT: {deltaT} ms")
             lastT = imu.timestamp
+            sample += 1
+            # Update the line plot.
+            y_acc[:, sample % num_timesteps] = (imu.ax, imu.ay, imu.az)
+
+            # Update Acc
+            uplot_handles[0].data = (x_data, *y_acc)
+
+            y_gyr[:, sample % num_timesteps] = (imu.gx, imu.gy, imu.gz)
+
+            # Update Gyro
+            uplot_handles[1].data = (x_data, *y_gyr)
 
 
 if __name__ == "__main__":
--- test/client/client_lidar.py
+++ test/client/client_lidar.py
@@ -1,16 +1,31 @@
 import argparse
 
 import grpc
+
+import viser
+import numpy as np
+
 from proto_gen import sensors_pb2_grpc, sensors_pb2
 
 
-DEFAULT_SERVER_ADDR = "192.168.3.251:50053"
+DEFAULT_SERVER_ADDR = "localhost:50051"
+
+
+def to_numpy(point_cloud: sensors_pb2.PointCloud3) -> np.ndarray:
+    arr = np.zeros((len(point_cloud), 3), dtype=np.float32)
+    for i, p in enumerate(point_cloud):
+        arr[i, 0] = p.x
+        arr[i, 1] = p.y
+        arr[i, 2] = p.z
+    return arr
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Stream LiDAR scans over gRPC.")
+    parser = argparse.ArgumentParser(description="Visualize LiDAR scans over gRPC.")
     parser.add_argument("--server", default=DEFAULT_SERVER_ADDR, help="gRPC target host:port")
     args = parser.parse_args()
+
+    visa_server = viser.ViserServer()
 
     with grpc.insecure_channel(args.server) as channel:
         stub = sensors_pb2_grpc.SensorServiceStub(channel)
@@ -22,6 +37,14 @@ def main():
             deltaT = scan.timestamp - last
             print(f" DeltaT: {deltaT} ms")
             last = scan.timestamp
+
+            visa_server.scene.add_point_cloud(
+                name="/lidar",
+                points=to_numpy(scan.points),
+                colors=(0, 0, 0),
+                point_size=0.05,
+                point_shape="rounded",
+            )
 
 
 if __name__ == "__main__":
